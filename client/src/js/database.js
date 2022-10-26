5dd95b239722e1adff555485ab31ea644edbc296
Randie <srandall1213@gmail.com>
2022-10-26 16:15:11 -0600

    updated sw & webpack code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,30 +14,23 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('content: ' + content)
-  console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ jate: content });
   const result = await request;
-  console.log('🚀 updated database', result);
-  console.error('putDb not implemented');
-}
+  console.log("Data saved to the database", result);
+};
 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET all from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
   const result = await request;
-  result
-    ? console.log('data retrieved from the database', result.value)
-    : console.log('data not found in the database');
-  return result?.value;
-}
+  console.log(result);
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,74 +3,72 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: 'development',
-    // Entry point for files
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
-      header: './src/js/header.js',
-      editor: './src/js/editor.js',
+      install: './src/js/install.js'
     },
-    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Jate'
+        template: "./index.html",
+        title: "JATE",
       }),
-     
-      // Injects our custom service worker
+      // INJECT MANIFEST
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
       }),
-
-      // Creates a manifest.json file.
+      // CREATES MANIFEST.JSON
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Jate',
-        short_name: 'Jate',
-        description: 'Just Another Text Editor',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "PWA text editor app",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
+      
     ],
 
     module: {
-      // CSS loaders
       rules: [
         {
-          test: /\.css$/i,
+          test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
-        },
+        }
       ],
     },
   };
